d0aa789baf782dfc004327df9f2e28787964dba9
NARESH <officialnaresh1103@gmail.com>
2024-08-22 23:09:49 +0530

    Adding constructor file into git

---
+++ contructor.java
@@ -0,0 +1,12 @@
+public class contructor {
+    void contructor(){
+        System.out.println("This is the constructor");
+
+    }
+    public  void main(String[] args) {
+        contructor c =new contructor();
+
+
+
+    }
+}
